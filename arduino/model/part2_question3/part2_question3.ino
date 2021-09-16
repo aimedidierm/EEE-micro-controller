1be69e506ca1fba95656c9c9915481eeced363a9
aimedidierm <aimedidiermugisha@gmail.com>
2021-09-16 21:51:19 +0200

    CAT2

--- arduino/model/part2_question3/part2_question3.ino
+++ arduino/model/part2_question3/part2_question3.ino
@@ -1,5 +1,5 @@
 #define Buzzer_pin 13
-#define speedmeter_pin 5
+#define speedometer_pin 5
 int speed_reading;
 int max_value=1023;
 int min_value=0;
@@ -11,5 +11,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  speed_reading = analogRead(speedometer_pin);
+  speed_reading = map(speed_reading, min_value, max_value,0,140);
+  constrain(speed_reading,0,140);
+  if(speed_reading>100){
+    digitalWrite(Buzzer_pin,HIGH);
+    delay(250);
+    digitalWrite(Buzzer_pin,LOW);
+    delay(250);
+    }
 
 }
